1ab3c1eb20c27867a91659d7ea97a1542dacb394
mundoenrique <mundoenrique@hotmail.com>
2022-07-04 12:41:31 -0400

    Proteciión de rutas

--- src/routers/AppRuoter.jsx
+++ src/routers/AppRuoter.jsx
@@ -1,14 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRouter } from './PrivateRouter';
+import { PublicRouter } from './PublicRouter';
 
 export default function AppRuoter() {
 	return (
 		<>
 			<Routes>
 				<Route path="/">
-					<Route index element={<LoginPage />} />
-					<Route path="*" element={<HeroesRoutes />} />
+					<Route
+						index
+						element={
+							<PublicRouter>
+								<LoginPage />
+							</PublicRouter>
+						}
+					/>
+					<Route
+						path="*"
+						element={
+							<PrivateRouter>
+								<HeroesRoutes />
+							</PrivateRouter>
+						}
+					/>
 				</Route>
 			</Routes>
 		</>
